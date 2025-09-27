4cf995b322265375ceb23a4b0ef8afcb1d8d908f
AstroHodgey <lachlion123@gmail.com>
2025-09-27 16:06:28 +1000

    Profile UI Commit

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -1,16 +1,33 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SubsystemsImplementation;
 
-public class Speedometer : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class Speedometer : MonoBehaviour {
+    [SerializeField] private GameObject heatPip;
+    private GameObject[] heatPips;
+
+    private int pipCount = 0;
+
+    public BikeMovement bikeMovement;
+    
+    void Start() {
+        heatPips = new GameObject[26];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (pipCount < bikeMovement.speed) {
+            pipCount++; heatPips[pipCount] = Instantiate(heatPip, this.transform);
+        }
+
+        if (pipCount - 1 > bikeMovement.speed) {
+            Destroy(transform.GetChild(pipCount).gameObject); pipCount--;
+        }
+
+        if (pipCount is >= 20 and < 22 && bikeMovement.speed > 20) {
+            pipCount++; heatPips[pipCount] = Instantiate(heatPip, this.transform);
+            pipCount++; heatPips[pipCount] = Instantiate(heatPip, this.transform);
+        }
     }
 }
